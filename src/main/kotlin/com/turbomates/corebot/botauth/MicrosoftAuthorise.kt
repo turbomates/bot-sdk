821a022717348384e529c3d6def78bd2d24a8cc2
anka <fedoruk.anna@gmail.com>
2020-01-14 16:48:28 +0300

    changes

--- src/main/kotlin/com/turbomates/corebot/botauth/MicrosoftAuthorise.kt
+++ src/main/kotlin/com/turbomates/corebot/botauth/MicrosoftAuthorise.kt
@@ -5,14 +5,21 @@ import io.ktor.client.request.forms.FormDataContent
 import io.ktor.http.Parameters
 
 class MicrosoftAuthorise(private val auth: BotAuth) {
+
+    companion object {
+        private const val URL = "https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token"
+        private const val SCOPE = "https://api.botframework.com/.default"
+        private const val GRANT_TYPE = "client_credentials"
+    }
+
     suspend fun get(): Token
     {
         val token = HttpClient().post<MicrosoftToken>(
-            "https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token",
+            URL,
             FormDataContent(
                 Parameters.build {
-                    append("grant_type", "client_credentials")
-                    append("scope", "https://api.botframework.com/.default")
+                    append("grant_type", GRANT_TYPE)
+                    append("scope", SCOPE)
                     append("client_id", auth.id)
                     append("client_secret", auth.password)
                 }
